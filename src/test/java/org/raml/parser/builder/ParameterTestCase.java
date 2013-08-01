f5ac4b463daa62a94c354793d71fb80d96f9b294
Santiago Vacas <svacas@gmail.com>
2013-08-01 14:56:53 -0700

    support file parameter type

--- src/test/java/org/raml/parser/builder/ParameterTestCase.java
+++ src/test/java/org/raml/parser/builder/ParameterTestCase.java
@@ -1,12 +1,18 @@
 package org.raml.parser.builder;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.raml.model.ActionType;
+import org.raml.model.ParamType;
 import org.raml.model.Raml;
+import org.raml.model.parameter.QueryParameter;
 import org.raml.model.parameter.UriParameter;
 import org.raml.parser.visitor.YamlDocumentBuilder;
 
@@ -16,12 +22,24 @@ public class ParameterTestCase
     @Test
     public void whenParameterIsYRequiredShouldBeTrue() throws IOException
     {
+        Raml raml = loadRaml();
+        UriParameter uriParameter = raml.getUriParameters().get("param2");
+        assertThat(uriParameter.isRequired(), is(true));
+    }
+
+    @Test
+    public void typeFile() throws Exception
+    {
+        Raml raml = loadRaml();
+        QueryParameter queryParameter = raml.getResources().get("/resource").getAction(ActionType.GET).getQueryParameters().get("param");
+        assertThat(queryParameter.getType(), is(ParamType.FILE));
+    }
+
+    private Raml loadRaml() throws IOException
+    {
         String simpleTest = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("org/raml/required-param.yaml"));
         YamlDocumentBuilder<Raml> ramlSpecBuilder = new YamlDocumentBuilder<Raml>(Raml.class);
-        Raml raml = ramlSpecBuilder.build(simpleTest);
-        
-        UriParameter uriParameter = raml.getUriParameters().get("param2");
-        Assert.assertThat(uriParameter.isRequired(), CoreMatchers.is(true));
+        return ramlSpecBuilder.build(simpleTest);
     }
 
 }
