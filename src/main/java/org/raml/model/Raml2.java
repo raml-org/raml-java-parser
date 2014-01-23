4401f75bea820baf7d36481889c43daa156ca339
pavel <kor@nika>
2014-01-23 18:16:23 +0700

    no visiting resource types and traits too

--- src/main/java/org/raml/model/Raml2.java
+++ src/main/java/org/raml/model/Raml2.java
@@ -50,6 +50,14 @@ public class Raml2 extends Raml {
 
 	public void visit(IRamlFileVisitor v) {
 		visitResources(this.getResources(), v);
+		Map<String, ResourceType> resourceTypesModel2 = getResourceTypesModel();
+		for (ResourceType z:resourceTypesModel2.values()){
+			v.visitResourceType(z);
+			visitResource(v, z);
+		}
+		for (TraitModel m:getTraitsModel().values()){
+			visitAction(v, m);
+		}
 	}
 	
 	public void addGlobalSchema(String name,String content,boolean json){
@@ -75,50 +83,58 @@ public class Raml2 extends Raml {
 	private void visitResources(Map<String, Resource> resourceTypesModel2,
 			IRamlFileVisitor v) {
 		for (Resource q : resourceTypesModel2.values()) {
-			boolean startVisit = v.startVisit(q);
-			if (startVisit) {
-				Map<String, UriParameter> uriParameters = q.getUriParameters();
-				for (String u:uriParameters.keySet()){
-					v.visit(u,uriParameters.get(u));
-				}
-				Map<ActionType, Action> actions = q.getActions();
-				for (Action a:actions.values()){
-					boolean startVisit2 = v.startVisit(a);
-					if (startVisit2){
-						Map<String, Header> headers = a.getHeaders();
-						for (String h:headers.keySet()){
-							v.visit(h,headers.get(h));
-						}
-						Map<String, QueryParameter> qp = a.getQueryParameters();
-						for (String h:qp.keySet()){
-							v.visit(h,qp.get(h));
-						}
-						v.startVisitBody();
-						Map<String, MimeType> body = a.getBody();
-						for (MimeType m:body.values()){
-							v.visit(m);
-						}
-						v.endVisitBody();
-						Map<String, Response> responses = a.getResponses();
-						for (String c:responses.keySet()){
-							Response response = responses.get(c);
-							v.startVisit(c,response);
-							for (MimeType m:response.getBody().values()){
-								v.visit(m);
-							}
-							Map<String, Header> headers2 = response.getHeaders();
-							for (String h:headers2.keySet()){
-								v.visit(h,headers2.get(h));
-							}							
-							v.endVisit(responses.get(c));
-						}
-					}
-					v.endVisit(a);
-				}
-				visitResources(q.getResources(), v);
-			}
-			v.endVisit(q);
+			visitResource(v, q);
 		}
+	}
+
+	private void visitResource(IRamlFileVisitor v, Resource q) {
+		boolean startVisit = v.startVisit(q);
+		if (startVisit) {
+			Map<String, UriParameter> uriParameters = q.getUriParameters();
+			for (String u:uriParameters.keySet()){
+				v.visit(u,uriParameters.get(u));
+			}
+			Map<ActionType, Action> actions = q.getActions();
+			for (Action a:actions.values()){
+				visitAction(v, a);
+			}
+			visitResources(q.getResources(), v);
+		}
+		v.endVisit(q);
+	}
+
+	private void visitAction(IRamlFileVisitor v, Action a) {
+		boolean startVisit2 = v.startVisit(a);
+		if (startVisit2){
+			Map<String, Header> headers = a.getHeaders();
+			for (String h:headers.keySet()){
+				v.visit(h,headers.get(h));
+			}
+			Map<String, QueryParameter> qp = a.getQueryParameters();
+			for (String h:qp.keySet()){
+				v.visit(h,qp.get(h));
+			}
+			v.startVisitBody();
+			Map<String, MimeType> body = a.getBody();
+			for (MimeType m:body.values()){
+				v.visit(m);
+			}
+			v.endVisitBody();
+			Map<String, Response> responses = a.getResponses();
+			for (String c:responses.keySet()){
+				Response response = responses.get(c);
+				v.startVisit(c,response);
+				for (MimeType m:response.getBody().values()){
+					v.visit(m);
+				}
+				Map<String, Header> headers2 = response.getHeaders();
+				for (String h:headers2.keySet()){
+					v.visit(h,headers2.get(h));
+				}							
+				v.endVisit(responses.get(c));
+			}
+		}
+		v.endVisit(a);
 	}
 
 }
