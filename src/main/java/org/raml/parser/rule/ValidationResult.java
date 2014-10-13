0572520162b63bb1c49e3b618951933713192db0
Paul Bober <pmbober@gmail.com>
2014-10-13 13:14:05 -0700

    reverted formatting

--- src/main/java/org/raml/parser/rule/ValidationResult.java
+++ src/main/java/org/raml/parser/rule/ValidationResult.java
@@ -23,104 +23,156 @@ import org.raml.parser.visitor.IncludeInfo;
 import org.yaml.snakeyaml.error.Mark;
 import org.yaml.snakeyaml.nodes.Node;
 
-public class ValidationResult {
+public class ValidationResult
+{
 
-	public static final int UNKNOWN = -1;
+    public static final int UNKNOWN = -1;
 
-	public enum Level {
-		ERROR, WARN, INFO
-	}
+    public enum Level
+    {
+        ERROR, WARN, INFO
+    }
 
-	private Level level;
-	private String message;
-	private int line;
-	private int startColumn;
-	private int endColumn;
-	private ContextPath contextPath;
-	private IncludeInfo extraIncludeInfo;
+    private Level level;
+    private String message;
+    private int line;
+    private int startColumn;
+    private int endColumn;
+    private ContextPath contextPath;
+    private IncludeInfo extraIncludeInfo;
 
-	private ValidationResult(Level level, String message, int line,
-			int startColumn, int endColumn) {
-		this.level = level;
-		this.message = message;
-		this.line = line;
-		this.startColumn = startColumn;
-		this.endColumn = endColumn;
-	}
+    private ValidationResult(Level level, String message, int line, int startColumn, int endColumn)
+    {
+        this.level = level;
+        this.message = message;
+        this.line = line;
+        this.startColumn = startColumn;
+        this.endColumn = endColumn;
+    }
 
-	public Level getLevel() {
-		return level;
-	}
+    public Level getLevel()
+    {
+        return level;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage()
+    {
+        return message;
+    }
 
-	public int getLine() {
-		return line;
-	}
+    public int getLine()
+    {
+        return line;
+    }
 
-	public int getStartColumn() {
-		return startColumn;
-	}
+    public int getStartColumn()
+    {
+        return startColumn;
+    }
 
-	public int getEndColumn() {
-		return endColumn;
-	}
+    public int getEndColumn()
+    {
+        return endColumn;
+    }
 
-	public boolean isValid() {
-		return level != Level.ERROR;
-	}
+    public boolean isValid()
+    {
+        return level != Level.ERROR;
+    }
 
-	public String getIncludeName() {
-		if (contextPath != null && contextPath.size() > 1) {
-			return contextPath.peek().getIncludeName();
-		}
-		return null;
-	}
+    public String getIncludeName()
+    {
+        if (contextPath != null && contextPath.size() > 1)
+        {
+            return contextPath.peek().getIncludeName();
+        }
+        return null;
+    }
 
-	public ContextPath getIncludeContext() {
-		return contextPath;
-	}
+    public ContextPath getIncludeContext()
+    {
+        return contextPath;
+    }
 
-	public void setIncludeContext(ContextPath contextPath) {
-		this.contextPath = new ContextPath(contextPath);
-		if (extraIncludeInfo != null) {
-			this.contextPath.push(extraIncludeInfo);
-			extraIncludeInfo = null;
-		}
-	}
+    public void setIncludeContext(ContextPath contextPath)
+    {
+        this.contextPath = new ContextPath(contextPath);
+        if (extraIncludeInfo != null)
+        {
+            this.contextPath.push(extraIncludeInfo);
+            extraIncludeInfo = null;
+        }
+    }
 
-	public void setExtraIncludeInfo(IncludeInfo extraIncludeInfo) {
-		this.extraIncludeInfo = extraIncludeInfo;
-	}
+    public void setExtraIncludeInfo(IncludeInfo extraIncludeInfo)
+    {
+        this.extraIncludeInfo = extraIncludeInfo;
+    }
 
-	public static boolean areValid(List<ValidationResult> validationResults) {
-		for (ValidationResult result : validationResults) {
-			if (!result.isValid()) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean areValid(List<ValidationResult> validationResults)
+    {
+        for (ValidationResult result : validationResults)
+        {
+            if (!result.isValid())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public static List<ValidationResult> getLevel(Level level,
-			List<ValidationResult> results) {
-		List<ValidationResult> filtered = new ArrayList<ValidationResult>();
-		for (ValidationResult result : results) {
-			if (result.level == level) {
-				filtered.add(result);
-			}
-		}
-		return filtered;
-	}
+    public static List<ValidationResult> getLevel(Level level, List<ValidationResult> results)
+    {
+        List<ValidationResult> filtered = new ArrayList<ValidationResult>();
+        for (ValidationResult result : results)
+        {
+            if (result.level == level)
+            {
+                filtered.add(result);
+            }
+        }
+        return filtered;
+    }
 
-	@Override
-	public String toString() {
-		return "ValidationResult{" + "level=" + level + ", message='" + message
-				+ '\'' + '}';
-	}
+    @Override
+    public String toString()
+    {
+        return "ValidationResult{" +
+               "level=" + level +
+               ", message='" + message + '\'' +
+               '}';
+    }
 
+    public static ValidationResult createErrorResult(String message, int line, int startIndex, int endIndex)
+    {
+        return new ValidationResult(Level.ERROR, message, line, startIndex, endIndex);
+    }
+
+    public static ValidationResult createErrorResult(String message, Mark startMark, Mark endMark)
+    {
+        return createErrorResult(message, startMark.getLine(), startMark.getColumn(), endMark.getColumn());
+    }
+
+    public static ValidationResult createErrorResult(String message, Node node)
+    {
+        return createErrorResult(message, node.getStartMark(), node.getEndMark());
+    }
+
+    public static ValidationResult createErrorResult(String message)
+    {
+        return createErrorResult(message, UNKNOWN, UNKNOWN, UNKNOWN);
+    }
+
+    public static ValidationResult createWarnResult(String message, Node node)
+    {
+        return new ValidationResult(Level.WARN, message, node.getStartMark().getLine(),
+                                    node.getStartMark().getColumn(), node.getEndMark().getColumn());
+    }
+
+    public static ValidationResult create(Level level, String message)
+    {
+        return new ValidationResult(level, message, UNKNOWN, UNKNOWN, UNKNOWN);
+    }
 	public String toDetailedString() {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("\t");
@@ -141,36 +193,5 @@ public class ValidationResult {
 			stringBuilder.append(")");
 		}
 		return stringBuilder.toString();
-	}
-
-	public static ValidationResult createErrorResult(String message, int line,
-			int startIndex, int endIndex) {
-		return new ValidationResult(Level.ERROR, message, line, startIndex,
-				endIndex);
-	}
-
-	public static ValidationResult createErrorResult(String message,
-			Mark startMark, Mark endMark) {
-		return createErrorResult(message, startMark.getLine(),
-				startMark.getColumn(), endMark.getColumn());
-	}
-
-	public static ValidationResult createErrorResult(String message, Node node) {
-		return createErrorResult(message, node.getStartMark(),
-				node.getEndMark());
-	}
-
-	public static ValidationResult createErrorResult(String message) {
-		return createErrorResult(message, UNKNOWN, UNKNOWN, UNKNOWN);
-	}
-
-	public static ValidationResult createWarnResult(String message, Node node) {
-		return new ValidationResult(Level.WARN, message, node.getStartMark()
-				.getLine(), node.getStartMark().getColumn(), node.getEndMark()
-				.getColumn());
-	}
-
-	public static ValidationResult create(Level level, String message) {
-		return new ValidationResult(level, message, UNKNOWN, UNKNOWN, UNKNOWN);
 	}
 }
