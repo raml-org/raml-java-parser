782eaad8949a9465bea9927f0b3ef7d268e3ef54
Federico Donnarumma <federico.donnarumma@mulesoft.com>
2015-11-23 15:47:20 -0300

    Fixing resolved uri parameters to fetch from parent

--- src/main/java/org/raml/model/Resource.java
+++ src/main/java/org/raml/model/Resource.java
@@ -169,7 +169,7 @@ public class Resource implements Serializable
     {
         if (parentResource != null)
         {
-            Map<String, UriParameter> uriParams = new HashMap<String, UriParameter>(parentResource.getUriParameters());
+            Map<String, UriParameter> uriParams = new HashMap<String, UriParameter>(parentResource.getResolvedUriParameters());
             uriParams.putAll(uriParameters);
             return uriParams;
         }
