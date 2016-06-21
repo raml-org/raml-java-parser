f3b84fd40fb1a07ed12afe741c3cee80a9eb8fb4
Miguel Oliva <moliva@users.noreply.github.com>
2016-06-21 17:34:35 -0300

    Generating XSD root element name accordingly (#117)

--- raml-parser-2/src/main/java/org/raml/v2/internal/impl/commons/model/type/TypeDeclaration.java
+++ raml-parser-2/src/main/java/org/raml/v2/internal/impl/commons/model/type/TypeDeclaration.java
@@ -15,6 +15,13 @@
  */
 package org.raml.v2.internal.impl.commons.model.type;
 
+import static java.util.Collections.singletonList;
+
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.apache.ws.commons.schema.XmlSchema;
 import org.raml.v2.api.loader.ResourceLoader;
 import org.raml.v2.internal.impl.commons.model.Annotable;
@@ -23,10 +30,12 @@ import org.raml.v2.internal.impl.commons.nodes.TypeDeclarationNode;
 import org.raml.v2.internal.impl.commons.nodes.TypeExpressionNode;
 import org.raml.v2.internal.impl.commons.type.ResolvedType;
 import org.raml.v2.internal.impl.commons.type.SchemaBasedResolvedType;
+import org.raml.v2.internal.impl.v10.nodes.NamedTypeExpressionNode;
 import org.raml.v2.internal.impl.v10.nodes.PropertyNode;
 import org.raml.v2.internal.impl.v10.phase.ExampleValidationPhase;
 import org.raml.v2.internal.impl.v10.type.AnyResolvedType;
 import org.raml.v2.internal.impl.v10.type.TypeToSchemaVisitor;
+import org.raml.v2.internal.impl.v10.type.XmlFacetsCapableType;
 import org.raml.yagi.framework.nodes.ErrorNode;
 import org.raml.yagi.framework.nodes.KeyValueNode;
 import org.raml.yagi.framework.nodes.Node;
@@ -34,14 +43,14 @@ import org.raml.yagi.framework.nodes.StringNode;
 import org.raml.yagi.framework.nodes.StringNodeImpl;
 import org.raml.yagi.framework.util.NodeSelector;
 
-import java.io.StringWriter;
-import java.util.Collections;
-import java.util.List;
-
-import static java.util.Collections.singletonList;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 public abstract class TypeDeclaration<T extends ResolvedType> extends Annotable
 {
+
+    private static final List<String> GLOBAL_NAMED_TYPE_DECLARATION_NODE_NAMES = Arrays.asList("types", "annotationTypes", "baseUriParameters");
+    private static final List<String> LOCAL_NAMED_TYPE_DECLARATION_NODE_NAMES = Arrays.asList("headers", "queryParameters", "queryString", "uriParameters");
 
     private KeyValueNode node;
     private T resolvedType;
@@ -134,10 +143,103 @@ public abstract class TypeDeclaration<T extends ResolvedType> extends Annotable
             return null;
         }
         final TypeToSchemaVisitor typeToSchemaVisitor = new TypeToSchemaVisitor();
-        typeToSchemaVisitor.transform(name(), getResolvedType());
+        typeToSchemaVisitor.transform(rootElementName(), getResolvedType());
         final XmlSchema schema = typeToSchemaVisitor.getSchema();
         final StringWriter writer = new StringWriter();
         schema.write(writer);
         return writer.toString();
+    }
+
+    private String rootElementName()
+    {
+        if (resolvedType instanceof XmlFacetsCapableType)
+        {
+            // an XML name facet overrides any other possibility for the root XML element
+            String xmlName = ((XmlFacetsCapableType) resolvedType).getXmlFacets().getName();
+            if (xmlName != null)
+            {
+                return xmlName;
+            }
+        }
+
+        if (!(node instanceof PropertyNode) && !isGlobalNamedTypeDeclarationNode() && !isLocalNamedTypeDeclarationNode())
+        {
+            // type reference
+            final Node value = node.getValue();
+            if (value.get("properties") != null || value.get("facets") != null || !(getTypeNode() instanceof NamedTypeExpressionNode))
+            {
+                // type is extending somehow supertype or inline declaration
+                return "root";
+            }
+            else
+            {
+                // type is just referencing an existing one
+                return ((NamedTypeExpressionNode) getTypeNode()).getRefName();
+            }
+        }
+        else
+        {
+            // type declaration
+            return name();
+        }
+    }
+
+    private Node getTypeNode()
+    {
+        final Node value = node.getValue();
+        final Node type = value.get("type");
+        if (type != null)
+        {
+            return type;
+        }
+        else
+        {
+            return value.get("schema");
+        }
+    }
+
+    /**
+     * True if the node is a global named type declaration as opposed to an inline/anonymous declaration. This is computed based on the parent of the current type declaration node, i.e.
+     * if its parent is one of the global named type declaration nodes possible.
+     * 
+     * @return <code>true</code> if the node is a global named type declaration as opposed to an inline/anonymous declaration, <code>false</code> otherwise
+     */
+    private boolean isGlobalNamedTypeDeclarationNode()
+    {
+        final Node parent = node.getParent();
+        final Node rootNode = node.getRootNode();
+
+        return Iterables.any(GLOBAL_NAMED_TYPE_DECLARATION_NODE_NAMES, new Predicate<String>()
+        {
+
+            @Override
+            public boolean apply(final String name)
+            {
+                return parent == rootNode.get(name);
+            }
+        });
+    }
+
+    /**
+     * True if the node is a local named type declaration as opposed to an inline/anonymous declaration. This is computed based on the parent of the current type declaration node, i.e.
+     * if its parent is one of the local named type declaration nodes possible.
+     * 
+     * @return <code>true</code> if the node is a local named type declaration as opposed to an inline/anonymous declaration, <code>false</code> otherwise
+     */
+    private boolean isLocalNamedTypeDeclarationNode()
+    {
+        final Node parent = node.getParent();
+        // tries to get the grandparent of the current node to retrieve the same node based on the known local named declaration node names
+        final Node baseNode = parent.getParent().getParent();
+
+        return Iterables.any(LOCAL_NAMED_TYPE_DECLARATION_NODE_NAMES, new Predicate<String>()
+        {
+
+            @Override
+            public boolean apply(final String name)
+            {
+                return parent == baseNode.get(name);
+            }
+        });
     }
 }
