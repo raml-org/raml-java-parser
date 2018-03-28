86aec941c2f013ca7ebd21342b1736de8360becb
pacafuentes <fuentespaca@gmail.com>
2018-03-28 15:33:11 -0300

    Fixes #478 NPE when using scheme under securityScheme. (#479)

---
+++ src/test/java/org/raml/parser/builder/SecuritySchemaTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 (c) MuleSoft, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.raml.parser.builder;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.raml.model.Raml;
+import org.raml.parser.rule.ValidationResult;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SecuritySchemaTestCase extends AbstractRamlTestCase{
+
+  private static final String ramlSource = "org/raml/security-scheme-schema.yaml";
+  private static Raml raml;
+
+  @BeforeClass
+  public static void init()
+  {
+    raml = parseRaml(ramlSource);
+  }
+
+  @Test
+  public void build()
+  {
+    assertThat(raml.getSecuritySchemes().size(), is(1));
+  }
+
+  @Test
+  public void validate()
+  {
+    List<ValidationResult> errors = validateRaml(ramlSource);
+    Assert.assertTrue("Errors must be empty", errors.isEmpty());
+  }
+}
