fdaa51ddd6bd8c2cdea3ba7f6afc3fe87b581ed9
Paca <fuentespaca@gmail.com>
2018-06-05 12:07:38 -0300

    Fixes #509 get string representation of type default value when is an… (#510)
    * Fixes #509 get string representation of type default value when is an object.
    
    * Changes based on comments, dump nodes as JSON.

--- raml-parser-2/src/test/java/org/raml/v2/api/SpecInterfacesV10TestCase.java
+++ raml-parser-2/src/test/java/org/raml/v2/api/SpecInterfacesV10TestCase.java
@@ -151,6 +151,8 @@ public class SpecInterfacesV10TestCase
         assertThat(parentTypesUser.get(0).name(), is("object"));
         assertThat(parentTypesUser.get(0).parentTypes(), hasSize(0));
 
+        assertThat(user.defaultValue(), is("{\n\"firstname\" : \"pietro\",\n\"lastname\" : \"maximoff\",\n\"age\" : 104,\n\"height\" : 1.563,\n\"goggles\" : true\n}"));
+
         // inherited object type
         ObjectTypeDeclaration superUser = (ObjectTypeDeclaration) types.get(1);
         assertThat(superUser.name(), is("SuperUser"));
@@ -169,6 +171,12 @@ public class SpecInterfacesV10TestCase
         assertThat(parentTypesSuperUserUser, hasSize(1));
         assertThat(parentTypesSuperUserUser.get(0).name(), is("object"));
         assertThat(parentTypesSuperUserUser.get(0).parentTypes(), hasSize(0));
+
+        ArrayTypeDeclaration users = (ArrayTypeDeclaration) types.get(2);
+        assertThat(users.name(), is("Users"));
+        assertThat(
+                users.defaultValue(),
+                is("[\n{\n\"firstname\" : \"pietro\",\n\"lastname\" : \"maximoff\",\n\"age\" : 104,\n\"height\" : 1.563,\n\"goggles\" : true\n},\n{\n\"firstname\" : \"lionel\",\n\"lastname\" : \"messi\",\n\"age\" : 30,\n\"height\" : 1.7,\n\"goggles\" : false\n}\n]"));
 
         // string type
         StringTypeDeclaration nString = (StringTypeDeclaration) types.get(3);
