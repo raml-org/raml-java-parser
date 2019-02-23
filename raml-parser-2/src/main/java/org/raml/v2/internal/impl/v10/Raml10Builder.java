5d038792fb079f1b1acbf30bed62724e0501ef06
mauriarroqui <mauriarroqui@gmail.com>
2019-02-23 16:53:17 -0300

    SE-10953 Poor performance

--- raml-parser-2/src/main/java/org/raml/v2/internal/impl/v10/Raml10Builder.java
+++ raml-parser-2/src/main/java/org/raml/v2/internal/impl/v10/Raml10Builder.java
@@ -53,8 +53,10 @@ import org.raml.yagi.framework.phase.TransformationPhase;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static org.raml.v2.api.model.v10.RamlFragment.Default;
@@ -68,6 +70,7 @@ public class Raml10Builder
 {
 
     private final Set<String> openedFiles = new HashSet<>();
+    private final Map<String, Node> alreadyBuilt = new HashMap<>();
 
     public Node build(String stringContent, RamlFragment fragment, ResourceLoader resourceLoader, String resourceLocation, int maxPhaseNumber) throws IOException
     {
@@ -76,6 +79,13 @@ public class Raml10Builder
 
     public Node build(Node contextNode, String stringContent, RamlFragment fragment, ResourceLoader resourceLoader, String resourceLocation, int maxPhaseNumber) throws IOException
     {
+        String key = resourceLocation + String.valueOf(stringContent.hashCode());
+        if (alreadyBuilt.containsKey(key))
+        {
+            System.out.println("Built hit: " + key);
+            return alreadyBuilt.get(key);
+        }
+
         if (openedFiles.contains(resourceLocation))
         {
 
@@ -105,6 +115,7 @@ public class Raml10Builder
                 rootNode = applyExtension(rootNode, resourceLoader, resourceLocation, fragment);
             }
             rootNode.annotate(new RamlVersionAnnotation(RAML_10));
+            alreadyBuilt.put(key, rootNode);
             return rootNode;
         }
         finally
