0e9f296cc043389caf5db6687b04dfdbc45f3d9f
jpbelang <jpbelang@gmail.com>
2019-09-15 15:22:00 -0400

    Quick check of plan.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,12 @@
 
 TRAVIS_TAG=$1
 
-echo $TRAVIS_TAG
+echo "${TRAVIS_TAG}-pre"
+date = `date`
+echo "amusement $date" > just_a_test.txt
+git add just_a_test.txt
+git commit -a -m "Testing $date"
+git push origin travis
 #mvn --settings ~/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG
 #mvn clean deploy --settings ~/settings.xml -DskipTests=true -B -U
 #mvn clean deploy --settings ~/settings.xml -DskipTests=true -Psonatype -B -U
