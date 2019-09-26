fb982c12f110b21a7658ebfe111a508189183f5e
jpbelang <jpbelang@gmail.com>
2019-09-25 21:35:29 -0400

    Fix for sonatype release.

--- scripts/open-next-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-NEW_RELEASE=$1
-mvn --settings ~/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$NEW_RELEASE && \
-   mvn clean install --settings ~/settings.xml -B -U || exit 127
-
-git commit -a -m "Preparing next release ${NEW_RELEASE}" && git push origin master || exit 127
--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-TRAVIS_TAG=$1
-mvn --settings ~/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG && \
-   mvn clean deploy --settings ~/settings.xml -DskipTests=true -B -U
