318ed2c050c950c1c77a8448348a86f9726cd2d4
jpbelang <jpbelang@gmail.com>
2020-01-08 20:10:30 -0500

    Final commit for #678

--- yagi/src/main/java/org/raml/yagi/framework/grammar/rule/BooleanTypeRule.java
+++ yagi/src/main/java/org/raml/yagi/framework/grammar/rule/BooleanTypeRule.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class BooleanTypeRule extends AbstractTypeRule
 {
-    private static final String STRICT_BOOLEANS = "org.raml.strict_booleans";
+    public static final String STRICT_BOOLEANS = "org.raml.strict_booleans";
 
     private final static String TRUE = "true";
     private final static String FALSE = "false";
